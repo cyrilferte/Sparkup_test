05850d6641e1d1247791e6cb96b5488f11382574
cyril ferte <cyril@fertec.fr>
2016-12-05 07:29:28 +0000

    add test of text in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,8 @@ class Contact < ApplicationRecord
     validates_email_format_of :email
     validates :email, presence: true, uniqueness: true
       
+      before_validation :format_string_fields
+      
     def self.savecontact(importation)
         xlsx = Roo::Spreadsheet.open importation.file
 
@@ -16,5 +18,12 @@ class Contact < ApplicationRecord
                 }
             contact = Contact.new data
         end
+    end
+    
+    def format_string_fields
+      self.first_name = first_name.gsub(/[^[[:alpha:]] ']+/, '')
+      self.last_name = last_name.gsub(/[^[[:alpha:]] ']+/, '')
+      self.first_name = first_name.titleize
+      self.last_name = last_name.titleize
     end
 end

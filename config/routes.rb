7a9fd362269dad28612025a7d963963be7d527ad
cyril ferte <cyril@fertec.fr>
2016-12-05 07:21:53 +0000

    create all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :importations, only: [ :new, :create, :show]
+    resources :contacts, only: [ :index]
+    root "importations#new"
 end

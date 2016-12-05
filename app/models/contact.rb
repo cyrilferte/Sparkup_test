ec7fb799396ac4a6bcb29ffc5e147e2a4d789a40
cyril ferte <cyril@fertec.fr>
2016-12-05 07:26:59 +0000

    init importation model and open and read data from file in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,20 @@
 class Contact < ApplicationRecord
+    validates :first_name, presence: true, length: { minimum: 3 }
+    validates :last_name, presence: true, length: { minimum: 3 }
+    validates_email_format_of :email
+    validates :email, presence: true, uniqueness: true
+      
+    def self.savecontact(importation)
+        xlsx = Roo::Spreadsheet.open importation.file
+
+        xlsx.each_row_streaming( pad_cells: true) do |row|
+            data = {
+                importation_id: importation.id,
+                first_name: row.first.value,
+                last_name: row[1].value,
+                email: row[2].value,
+                }
+            contact = Contact.new data
+        end
+    end
 end
